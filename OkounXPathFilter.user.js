6616fd5cdf08dd9573846e6ec2abc63074de6c9f
lukas-hosek <47420264+lukas-hosek@users.noreply.github.com>
2022-09-13 21:21:42 +0200

    Update OkounXPathFilter.user.js

--- OkounXPathFilter.user.js
+++ OkounXPathFilter.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         Echelon's okoun xpath filter
+// @name         echelon's xpath filter
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Filtr ritnych.dier na okounu
+// @version      0.2
+// @description  try to take over the world!
 // @author       You
 // @match        https://www.okoun.cz/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=okoun.cz
+// @updateURL    https://raw.githubusercontent.com/lukas-hosek/OkounXPathFilter/main/OkounXPathFilter.user.js#bypass=true
 // @grant        none
 // ==/UserScript==
 
@@ -15,7 +16,7 @@
     let ultradebilove = ['adijunkt', 'Bernhard_Weiss', 'bmn', 'Brandenburg', 'Branimir', 'bretislav.jonas', 'd.smiricky', 'Dagobert_Durr', 'Das_Reich', 'florian_geyer', 'frantisek.kachna', 'Gotz_Berlichingen', 'Hajny_Filiburg', 'hamacek', 'Handschar', 'Hilfswilliger', 'horacek', 'Horst_Wessel', 'Charlemagne', 'Charlemagne_', 'Isidor', 'Januar', 'jarda.dusek', 'jasanek', 'Jurij_Ozerov', 'Kama', 'Karstjager', 'Koprovka', 'Knour', 'Kpt_Tuma', 'Landstorm_Netherland', 'Langemarck', 'Laser_eye', 'Lutzow', 'maqeo.cz', 'Maria_Theresia', 'mazurek', 'mazanej_lucifer', 'Mudrford', 'Neknubak', 'Nibelungen', 'Nord_', 'Norland', 'OberSturmKlippFurher', 'Oblazek', 'piANistka', 'Plch', 'Plsik_Liskovy', 'Polizei', 'pixicz', 'Prinz_Eugen', 'profesor_Birkermaier', 'Protez_alpska', 'prucha', 'ritna.diera', 'vojin.kouba', 'vonavka', 'Wallonien', 'Zufanek'];
 
     // construct xpath query, selects all divs that contain <span class='user'>ultradebil</span>
-    let xPath = '//div[@class=\'list posted-item-list articles\']/div/div/span[@class=\'user\' and (text() = \'' + ultradebilove[0] + '\'';
+    let xPath = '//span[@class=\'user\' and (text() = \'' + ultradebilove[0] + '\'';
     for (let i = 1; i < ultradebilove.length; ++i)
     {
         xPath += ' or text() = \'' + ultradebilove[i] + '\'';
