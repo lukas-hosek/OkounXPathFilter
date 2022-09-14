1acc1daccae88d45a8ab8e5f8631bcb83063f013
lukas-hosek <47420264+lukas-hosek@users.noreply.github.com>
2022-09-14 19:56:28 +0200

    Zastavení loadování obrázků

--- OkounAdminXPathFilter.user.js
+++ OkounAdminXPathFilter.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         echelon's admin xpath filter
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  try to take over the world!
-// @author       You
+// @version      0.3
+// @description  blocks and deletes unwanted posts from okoun.cz
+// @author       echelon
 // @match        https://www.okoun.cz/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=okoun.cz
 // @downloadURL  https://github.com/lukas-hosek/OkounXPathFilter/raw/main/OkounAdminXPathFilter.user.js
@@ -12,31 +12,27 @@
 // ==/UserScript==
 
 
-const ultradebilove = ['adijunkt', 'Bernhard_Weiss', 'bmn', 'Brandenburg', 'Branimir', 'bretislav.jonas', 'd.smiricky', 'Dagobert_Durr', 'Das_Reich', 'florian_geyer', 'frantisek.kachna', 'Gotz_Berlichingen', 'Hajny_Filiburg', 'hamacek', 'Handschar', 'Hilfswilliger', 'horacek', 'Horst_Wessel', 'Charlemagne', 'Charlemagne_', 'Isidor', 'Januar', 'jarda.dusek', 'jasanek', 'Jurij_Ozerov', 'Kama', 'Karstjager', 'Koprovka', 'Knour', 'Kpt_Tuma', 'Landstorm_Netherland', 'Langemarck', 'Laser_eye', 'Lutzow', 'maqeo.cz', 'Maria_Theresia', 'mazurek', 'mazanej_lucifer', 'Mudrford', 'Neknubak', 'Nibelungen', 'Nord_', 'Norland', 'OberSturmKlippFurher', 'Oblazek', 'piANistka', 'Plch', 'Plsik_Liskovy', 'Polizei', 'pixicz', 'Prinz_Eugen', 'profesor_Birkermaier', 'Protez_alpska', 'prucha', 'ritna.diera', 'vojin.kouba', 'vonavka', 'Wallonien', 'Zufanek'];
+const ultradebilove = ['testovaci.kakes', 'adijunkt', 'Bernhard_Weiss', 'bmn', 'Brandenburg', 'Branimir', 'bretislav.jonas', 'd.smiricky', 'Dagobert_Durr', 'Das_Reich', 'florian_geyer', 'frantisek.kachna', 'Gotz_Berlichingen', 'Hajny_Filiburg', 'hamacek', 'Handschar', 'Hilfswilliger', 'horacek', 'Horst_Wessel', 'Charlemagne', 'Charlemagne_', 'Isidor', 'Januar', 'jarda.dusek', 'jasanek', 'Jurij_Ozerov', 'Kama', 'Karstjager', 'Koprovka', 'Knour', 'Kpt_Tuma', 'Landstorm_Netherland', 'Langemarck', 'Laser_eye', 'Lutzow', 'maqeo.cz', 'Maria_Theresia', 'mazurek', 'mazanej_lucifer', 'Mudrford', 'Neknubak', 'Nibelungen', 'Nord_', 'Norland', 'OberSturmKlippFurher', 'Oblazek', 'piANistka', 'Plch', 'Plsik_Liskovy', 'Polizei', 'pixicz', 'Prinz_Eugen', 'profesor_Birkermaier', 'Protez_alpska', 'prucha', 'ritna.diera', 'vojin.kouba', 'vonavka', 'Wallonien', 'Zufanek'];
+
 
 function deletePosts()
 {
-    // construct xpath query, selects all divs that contain <span class='user'>ultradebil</span>
+    // Construct xpath query, selects all delete checkboxes in posts by users in the ban list
     let xPath = '//div[@class=\'meta\']/span[@class=\'user\' and (text() = \'' + ultradebilove[0] + '\'';
     for (let i = 1; i < ultradebilove.length; ++i)
     {
         xPath += ' or text() = \'' + ultradebilove[i] + '\'';
     }
     xPath += ')]/../span[@class=\'delete\']/input';
-    //xPath += ')]/..';
-
-    console.log(xPath);
 
     let xPathResult = document.evaluate(xPath, document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
-    console.log(xPathResult.snapshotLength);
-
     if (xPathResult.snapshotLength > 0)
     {
+        // Click all checkboxes
         for (let i = 0; i < xPathResult.snapshotLength; ++i)
         {
             let checkbox = xPathResult.snapshotItem(i);
-
             checkbox.click();
         }
 
@@ -44,7 +40,7 @@ function deletePosts()
         let deleteForm = document.evaluate(xPathDeleteForm, document.body, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
         if (deleteForm)
         {
-
+            // Create an invisible iframe in which the deletion form will be executed
             let docBody = document.getElementById('body');
             let iframe = document.createElement('iframe');
             iframe.height = '1';
@@ -52,17 +48,38 @@ function deletePosts()
             iframe.name='deletionIframe';
             iframe.id = 'deletionIframe';
             docBody.appendChild(iframe);
-
+            let oldTarget = deleteForm.target;
             deleteForm.target='deletionIframe';
             deleteForm.submit();
+            // Restore old form target so that delete works as usual
+            deleteForm.target = oldTarget;
         }
-
     }
 }
 
+function stopImages()
+{
+    // Construct xpath query, selects all imgs in posts posted by people in the ban list
+    let xPath = '//span[@class=\'user\' and (text() = \'' + ultradebilove[0] + '\'';
+    for (let i = 1; i < ultradebilove.length; ++i)
+    {
+        xPath += ' or text() = \'' + ultradebilove[i] + '\'';
+    }
+    xPath += ')]/../..//img';
+    let xPathResult = document.evaluate(xPath, document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+    // Set img.src to an empty string to stop loading. Works in Firefox.
+    for (let i = xPathResult.snapshotLength-1; i >= 0; --i)
+    {
+        let elem = xPathResult.snapshotItem(i);
+        elem.src='';
+    }
+}
+
+
 function hidePosts()
 {
-    // construct xpath query, selects all divs that contain <span class='user'>ultradebil</span>
+    // Construct xpath query, selects all divs that contain <span class='user'>ultradebil</span>
     let xPath = '//span[@class=\'user\' and (text() = \'' + ultradebilove[0] + '\'';
     for (let i = 1; i < ultradebilove.length; ++i)
     {
@@ -83,6 +100,7 @@ function hidePosts()
 (function() {
     'use strict';
 
-    deletePosts(ultradebilove);
-    hidePosts(ultradebilove);
+    deletePosts();
+    stopImages();
+    hidePosts();
 })();
--- OkounXPathFilter.user.js
+++ OkounXPathFilter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         echelon's xpath filter
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.okoun.cz/*
@@ -11,12 +11,31 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+const ultradebilove = ['testovaci.kakes', 'adijunkt', 'Bernhard_Weiss', 'bmn', 'Brandenburg', 'Branimir', 'bretislav.jonas', 'd.smiricky', 'Dagobert_Durr', 'Das_Reich', 'florian_geyer', 'frantisek.kachna', 'Gotz_Berlichingen', 'Hajny_Filiburg', 'hamacek', 'Handschar', 'Hilfswilliger', 'horacek', 'Horst_Wessel', 'Charlemagne', 'Charlemagne_', 'Isidor', 'Januar', 'jarda.dusek', 'jasanek', 'Jurij_Ozerov', 'Kama', 'Karstjager', 'Koprovka', 'Knour', 'Kpt_Tuma', 'Landstorm_Netherland', 'Langemarck', 'Laser_eye', 'Lutzow', 'maqeo.cz', 'Maria_Theresia', 'mazurek', 'mazanej_lucifer', 'Mudrford', 'Neknubak', 'Nibelungen', 'Nord_', 'Norland', 'OberSturmKlippFurher', 'Oblazek', 'piANistka', 'Plch', 'Plsik_Liskovy', 'Polizei', 'pixicz', 'Prinz_Eugen', 'profesor_Birkermaier', 'Protez_alpska', 'prucha', 'ritna.diera', 'vojin.kouba', 'vonavka', 'Wallonien', 'Zufanek'];
 
-    let ultradebilove = ['adijunkt', 'Bernhard_Weiss', 'bmn', 'Brandenburg', 'Branimir', 'bretislav.jonas', 'd.smiricky', 'Dagobert_Durr', 'Das_Reich', 'florian_geyer', 'frantisek.kachna', 'Gotz_Berlichingen', 'Hajny_Filiburg', 'hamacek', 'Handschar', 'Hilfswilliger', 'horacek', 'Horst_Wessel', 'Charlemagne', 'Charlemagne_', 'Isidor', 'Januar', 'jarda.dusek', 'jasanek', 'Jurij_Ozerov', 'Kama', 'Karstjager', 'Koprovka', 'Knour', 'Kpt_Tuma', 'Landstorm_Netherland', 'Langemarck', 'Laser_eye', 'Lutzow', 'maqeo.cz', 'Maria_Theresia', 'mazurek', 'mazanej_lucifer', 'Mudrford', 'Neknubak', 'Nibelungen', 'Nord_', 'Norland', 'OberSturmKlippFurher', 'Oblazek', 'piANistka', 'Plch', 'Plsik_Liskovy', 'Polizei', 'pixicz', 'Prinz_Eugen', 'profesor_Birkermaier', 'Protez_alpska', 'prucha', 'ritna.diera', 'vojin.kouba', 'vonavka', 'Wallonien', 'Zufanek'];
+function stopImages()
+{
+    // Construct xpath query, selects all imgs in posts posted by people in the ban list
+    let xPath = '//span[@class=\'user\' and (text() = \'' + ultradebilove[0] + '\'';
+    for (let i = 1; i < ultradebilove.length; ++i)
+    {
+        xPath += ' or text() = \'' + ultradebilove[i] + '\'';
+    }
+    xPath += ')]/../..//img';
+    let xPathResult = document.evaluate(xPath, document.body, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
-    // construct xpath query, selects all divs that contain <span class='user'>ultradebil</span>
+    // Set img.src to an empty string to stop loading. Works in Firefox.
+    for (let i = xPathResult.snapshotLength-1; i >= 0; --i)
+    {
+        let elem = xPathResult.snapshotItem(i);
+        elem.src='';
+    }
+}
+
+
+function hidePosts()
+{
+    // Construct xpath query, selects all divs that contain <span class='user'>ultradebil</span>
     let xPath = '//span[@class=\'user\' and (text() = \'' + ultradebilove[0] + '\'';
     for (let i = 1; i < ultradebilove.length; ++i)
     {
@@ -32,5 +51,11 @@
         let elem = xPathResult.snapshotItem(i);
         elem.parentNode.removeChild(elem);
     }
+}
 
+(function() {
+    'use strict';
+
+    stopImages();
+    hidePosts();
 })();
