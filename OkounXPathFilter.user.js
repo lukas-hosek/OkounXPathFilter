cfbd1bf93e458b4d9b16cdc375a527bc3ec4e6a0
lukas-hosek <47420264+lukas-hosek@users.noreply.github.com>
2022-09-15 00:57:38 +0200

    Updatnuta nemazací verze

--- OkounXPathFilter.user.js
+++ OkounXPathFilter.user.js
@@ -1,17 +1,22 @@
 // ==UserScript==
 // @name         echelon's xpath filter
 // @namespace    http://tampermonkey.net/
-// @version      0.4
-// @description  try to take over the world!
-// @author       You
+// @version      0.5
+// @description  blocks and deletes unwanted posts from okoun.cz
+// @author       echelon
 // @match        https://www.okoun.cz/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=okoun.cz
-// @downloadURL  https://raw.githubusercontent.com/lukas-hosek/OkounXPathFilter/main/OkounXPathFilter.user.js#bypass=true
-// @updateURL    https://raw.githubusercontent.com/lukas-hosek/OkounXPathFilter/main/OkounXPathFilter.user.js#bypass=true
-// @grant        none
+// @downloadURL  https://github.com/lukas-hosek/OkounXPathFilter/raw/main/OkounAdminXPathFilter.user.js
+// @updateURL    https://github.com/lukas-hosek/OkounXPathFilter/raw/main/OkounAdminXPathFilter.user.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @require      https://github.com/StigNygaard/GMCommonAPI.js/raw/master/GMCommonAPI.js
 // ==/UserScript==
 
-const ultradebilove = ['testovaci.kakes', 'adijunkt', 'Bernhard_Weiss', 'bmn', 'Brandenburg', 'Branimir', 'bretislav.jonas', 'd.smiricky', 'Dagobert_Durr', 'Das_Reich', 'florian_geyer', 'frantisek.kachna', 'Gotz_Berlichingen', 'Hajny_Filiburg', 'hamacek', 'Handschar', 'Hilfswilliger', 'horacek', 'Horst_Wessel', 'Charlemagne', 'Charlemagne_', 'Isidor', 'Januar', 'jarda.dusek', 'jasanek', 'Jurij_Ozerov', 'Kama', 'Karstjager', 'Koprovka', 'Knour', 'Kpt_Tuma', 'Landstorm_Netherland', 'Langemarck', 'Laser_eye', 'Lutzow', 'maqeo.cz', 'Maria_Theresia', 'mazurek', 'mazanej_lucifer', 'Mudrford', 'Neknubak', 'Nibelungen', 'Nord_', 'Norland', 'OberSturmKlippFurher', 'Oblazek', 'piANistka', 'Plch', 'Plsik_Liskovy', 'Polizei', 'pixicz', 'Prinz_Eugen', 'profesor_Birkermaier', 'Protez_alpska', 'prucha', 'ritna.diera', 'vojin.kouba', 'vonavka', 'Wallonien', 'Zufanek'];
+
+const defaultBanList = 'adijunkt, Bernhard_Weiss, bmn, Brandenburg, Bloodrot, Branimir, bretislav.jonas, d.smiricky, Dagobert_Durr, Das_Reich, florian_geyer, frantisek.kachna, Gotz_Berlichingen, Hajny_Filiburg, hamacek, Handschar, Hilfswilliger, horacek, Horst_Wessel, Charlemagne, Charlemagne_, Isidor, Januar, jarda.dusek, jasanek, Jurij_Ozerov, Kama, Karstjager, Koprovka, Knour, Kpt_Tuma, Landstorm_Netherland, Langemarck, Laser_eye, Lutzow, maqeo.cz, Maria_Theresia, mazurek, mazanej_lucifer, Mudrford, Neknubak, Nibelungen, Nord_, Norland, OberSturmKlippFurher, Oblazek, piANistka, Plch, Plsik_Liskovy, Polizei, pixicz, Prinz_Eugen, profesor_Birkermaier, Protez_alpska, prucha, ritna.diera, vojin.kouba, vonavka, Wallonien, Zufanek';
+var ultradebilove;
+
 
 function stopImages()
 {
@@ -53,9 +58,60 @@ function hidePosts()
     }
 }
 
+async function parseBanList(response)
+{
+    if (!response.ok)
+    {
+        return;
+    }
+    let responseText = await response.text();
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(responseText, 'text/html');
+    if (doc.querySelector('parseerror'))
+    {
+        return;
+    }
+    let xdata = doc.getElementsByClassName('xdata');
+    if (xdata.length != 1)
+    {
+        return;
+    }
+    let banArray = xdata[0].textContent.split(', ');
+    if (banArray.length < 5)
+    {
+        // just a sanity check
+        return;
+    }
+    GMC.setValue('bannedUsers', xdata[0].textContent);
+    console.log(`Banlist updated, ${banArray.length} records`);
+}
+
+
+function updateBanList()
+{
+  	let updateTimestamp = parseInt(GMC.getValue('updateTimestamp', 0));
+    let currentTimestamp = Date.now();
+  	let updateInterval = 1 * 60 * 60 * 1000;
+    if (updateTimestamp + updateInterval > currentTimestamp)
+    {
+        // List is sufficiently up to date
+      return;
+    }
+    GMC.setValue('updateTimestamp', currentTimestamp);
+
+    fetch('https://www.okoun.cz/boards/seznam_parazitu').then(res => parseBanList(res));
+}
+
+
 (function() {
     'use strict';
 
+    let listOfBannedUsers = GMC.getValue('bannedUsers', defaultBanList);
+    ultradebilove = listOfBannedUsers.split(', ');
+    console.log(`${ultradebilove.length} records`);
+
     stopImages();
     hidePosts();
+
+    updateBanList();
 })();
